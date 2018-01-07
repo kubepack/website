d4abbb3f04ef5dec8af77e082027abc568872e6d
Sajib Khan <sajibcse68@gmail.com>
2018-01-07 20:53:00 +0600

    Modify docs-aggregator stuffs (#4)

--- docs-aggregator.js
+++ docs-aggregator.js
@@ -1,43 +1,80 @@
-const jsonFile = require('jsonfile'); const productFile = './products.json'; const shell = require('shelljs'); const path = require('path'); jsonFile.readFile(productFile, 
-function(err, obj) {
+#!/usr/bin/env node
+
+const jsonFile = require('jsonfile');
+const productFile = './products.json';
+const shell = require('shelljs');
+const path = require('path');
+const replace = require('replace');
+const fse = require('fs-extra');
+
+jsonFile.readFile(productFile, function(err, obj) {
   const allProducts = obj.products;
-  // clean repos/
-  shell.exec('rm -rf ./repos/');
+
   for (const key in allProducts) {
-    if (allProducts.hasOwnProperty(key)) {
+    // clean
+    shell.exec('rm -rf ./repos/' + key);
+
+  if (allProducts.hasOwnProperty(key)) {
       // clone repo
       const url = allProducts[key].url;
       const repoName = key;
       shell.exec('git clone ' + url + ' ./repos/' + repoName);
-      const docsDestRoot = path.join('../../content/docs');
+      const docsDestRoot = path.join('./content/docs');
       const versions = allProducts[repoName].versions;
       const versionsLen = versions.length;
       for (let j = 0; j < versionsLen; j++) {
-        if (versions[j].isDocsAvailable) {
+        if (versions[j].hostDocs) {
           const branch = versions[j].branch;
           console.log('branch: ......', branch);
-          shell.exec('pwd');
           shell.pushd(path.join('.', 'repos', repoName));
           shell.exec('git checkout ' + branch);
+          shell.popd();
 
-          // install 'replace' globally ($ npm i -g replace)
           const replaceWith = path.join('(/docs', branch);
-          console.log('>>>>>replaceWith: ', replaceWith);
-          shell.exec("replace '(\\(/docs)' '" + replaceWith + "' docs -r");
-          shell.exec("replace '.md\\)' ')' docs -r");
-          // shell.exec('git log -2');
+
+          replace({
+            regex: '(\\(/docs)',
+            replacement: `${replaceWith}`,
+            paths: [`./repos/${repoName}/docs/`],
+            recursive: true,
+            silent: true
+          });
+
+          replace({
+            regex: '.md\\)',
+            replacement: ')',
+            paths: [`./repos/${repoName}/docs/`],
+            recursive: true,
+            silent: true
+          });
+
+          replace({
+            regex: '/docs/images',
+            replacement: `/products/${repoName}/${branch}/images`,
+            paths: [`./repos/${repoName}/docs/`],
+            recursive: true,
+            silent: true
+          });
+
           // copy docs into content/docs/<version>/
           const docsDestDir = path.join(docsDestRoot, branch);
           // delete the dir if exist
-          shell.exec('rm -rf ' + docsDestDir);
-          shell.exec('mkdir -p ' + docsDestDir);
-          shell.exec('cp -rf docs/ ' + docsDestDir);
+          fse.removeSync(docsDestDir);
+          // create the dir
+          fse.ensureDirSync(docsDestDir);
+          try {
+            fse.copySync(`./repos/${repoName}/docs/`, `./content/docs/${branch}/`);
+            console.log('!!success!!');
+          } catch (error) {
+            console.log('=======error: ', error);
+          }
+          shell.pushd(path.join('.', 'repos', repoName));
           shell.exec('git checkout -- .');
           shell.popd();
         }
       }
       // clean
-      // shell.exec('rm -rf ./repos/' + key);
+      shell.exec('rm -rf ./repos/' + key);
     }
   }
 });
